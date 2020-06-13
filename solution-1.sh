0480dd2594cd47d34d1261231faf31a47ba57b54
joebadmus <joebadmus@gmail.com>
2020-06-13 09:31:59 +0100

    Added solution 1 script

---
+++ solution-1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# This exercise will help you to create your first script.
+# Write a script using your favorite editor. The script should display the path to your homedirectory and the terminal type that you are using.)
+# Add comments in your script.
+# Add information for the users of your script.
+# Change permissions on your script so that you can run it.
+# Run the script in normal mode and in debug mode. It should run without errors.
+
+# Display path to user's home directory
+echo "Your home directory is: ${HOME}"
+
+# Display terminal type
+echo "Your terminal type is: ${SHELL}"
